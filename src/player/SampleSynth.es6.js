bf0ded26f6353bb2787744d60a8dbc9516cd2d0f
Norbert Schnell <Norbert.Schnell@ircam.fr>
2015-03-20 12:55:50 +0100

    restored synth that maps cross-fade between two sounds on touch x

--- src/player/SampleSynth.es6.js
+++ src/player/SampleSynth.es6.js
@@ -15,23 +15,61 @@ class SampleSynth {
     this.output.gain.value = 1;
   }
 
-  trigger(time, params, transformed = false) {
-    var x = params.x || 0.5;
-    var y = params.y || 0.5;
-    var index = Math.floor((1 - y) * 12);
-    var level = transformed ? 1 : 0;
-    var buffer = this.audioBuffers[2 * index + level];
+  // trigger(time, params, echo = false) {
+  //   var x = params.x || 0.5;
+  //   var y = params.y || 0.5;
+  //   var index = Math.floor((1 - y) * 12);
+  //   var level = echo ? 1 : 0;
+  //   var buffer = this.audioBuffers[2 * index + level];
 
-    var g = audioContext.createGain();
-    g.connect(this.output);
-    g.gain.value = params.gain;
+  //   var g = audioContext.createGain();
+  //   g.connect(this.output);
+  //   g.gain.value = params.gain;
 
-    var s = audioContext.createBufferSource();
-    s.buffer = buffer;
-    s.connect(g);
-    s.start(time);
+  //   var s = audioContext.createBufferSource();
+  //   s.buffer = buffer;
+  //   s.connect(g);
+  //   s.start(time);
 
-    return buffer.duration;
+  //   return buffer.duration;
+  // }
+
+  trigger(time, params, echo = false) {
+    var audioBuffers = this.audioBuffers;
+    var duration = 0;
+
+    if (audioBuffers && audioBuffers.length > 0) {
+      var x = params.x || 0.5;
+      var y = params.y || 0.5;
+
+      var index = Math.floor((1 - y) * 12);
+
+      var b1 = audioBuffers[2 * index];
+      duration += (1 - x) * b1.duration;
+
+      var g1 = audioContext.createGain();
+      g1.connect(this.output);
+      g1.gain.value = (1 - x) * params.gain;
+
+      var s1 = audioContext.createBufferSource();
+      s1.buffer = b1;
+      s1.connect(g1);
+      s1.start(time);
+
+      var b2 = audioBuffers[2 * index + 1];
+      duration += x * b2.duration;
+
+      var g2 = audioContext.createGain();
+      g2.connect(this.output);
+      g2.gain.value = x * params.gain;
+
+      var s2 = audioContext.createBufferSource();
+      s2.buffer = b2;
+      s2.connect(g2);
+      s2.start(time);
+    }
+
+    return duration;
   }
 }
 
