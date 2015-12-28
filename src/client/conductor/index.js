8b57d6567bd297ea1acef1bd2ac528bd2298b7d7
b-ma <benjamin.matuszewski@gmail.com>
2015-12-28 15:25:54 +0100

    updated GUI

--- src/client/conductor/index.js
+++ src/client/conductor/index.js
@@ -8,5 +8,9 @@ window.addEventListener('load', () => {
   client.init('conductor');
   const control = new Control({ hasGui: true });
 
+  control.configureGui('state', { type: 'buttons' });
+  control.configureGui('loopAttenuation', { type: 'slider', size: 'large' });
+  control.configureGui('minGain', { type: 'slider', size: 'large' });
+
   client.start(control);
 });
